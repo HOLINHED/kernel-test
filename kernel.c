5450c263f0d8c55720912bbe3cf807c4b9fe38cb
Dawid Klatkiewicz <klatkiewiczdawid@gmail.com>
2020-07-26 03:40:05 -0400

    function to print easier

--- kernel.c
+++ kernel.c
@@ -1,19 +1,31 @@
-kmain() {
-   char* vidmem = (char*)0xb8000;
-   
-   for (int i = 0; i < 256 * 70; i++) {
-      vidmem[i] = ' ';
-      vidmem[i + 1] = 0x00;
-   }
+static char* vidmem = (char*)0xb8000;
+static int vidmem_b = 0;
 
-   vidmem[0] = 'H';
-   vidmem[1] = 0x07;
-   vidmem[2] = 'E';
-   vidmem[3] = 0x07;
-   vidmem[4] = 'L';
-   vidmem[5] = 0x07;
-   vidmem[6] = 'L';
-   vidmem[7] = 0x07;
-   vidmem[8] = 'O';
-   vidmem[9] = 0x07;
+int strlen(const char* str) {
+   int size = 0;
+   while (str[size] != '\0') size++;
+   return size;
+}
+
+void clear_buffer() {
+   for (int i = 0; i < 25 * 80 * 2; i += 2) {
+      if (i != 0) vidmem[i] = ' ';
+      else vidmem[i] = 'X';
+      vidmem[i + 1] = 0x07;
+   }
+}
+
+void push_buffer(char* message) {
+   int m_size = strlen(message);
+
+   for (int i = vidmem_b, j = 0; i < m_size * 2; i += 2, j++) {
+      vidmem[i] = message[j];
+      vidmem[i + 1] = 0x07;
+   }
+}
+
+kmain() {
+   clear_buffer();
+   push_buffer("Hello, world!");
+
 }
